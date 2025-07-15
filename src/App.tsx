4d956b4c1658ed2d800d2389a791ac5eb7707420
Nikesha Silva <nikesha.silva@innovative-e.com>
2025-07-15 21:46:08 +0530

    Added project startup dependencies

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { NewAppScreen } from '@react-native/new-app-screen';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+});
+
+export default App;
